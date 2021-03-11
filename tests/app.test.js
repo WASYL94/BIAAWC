080cfd0ec8229673bd18aa9155a06e5afe4c5e37
WASYL94 <adminwroc@gmail.com>
2021-03-11 23:32:58 +0100

    first commit

---
+++ tests/app.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('Test the root path', () => {
+    test('It should response the GET method', (done) => {
+        request(app)
+            .get('/')
+            .then(resp => {
+                expect(resp.statusCode).toBe(200)
+                expect(resp.json).toBe('Hello World!')
+                done()
+            })
+            .catch(e => {
+                done()
+            });
+    });
+});
+
+// function sum (a, b) {
+//     return a + b;
+// }
+
+// describe('Tests of a calculator functionality', () => {
+//     test('sum two numbers', () => {
+//         const result = sum(1, 5)
+
+//         expect(6).toEqual(result)
+//     });
+
+
+// });
+
+// describe('Tests of a calculator functionality', () => {
+//     it('Should sum two numbers', () => {
+
+//     });
+
+//     it('Should div two numbers', () => {
+
+//     });
+// });
